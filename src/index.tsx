3609c77cb7c3ea05b3633fb4c0ab2fe3a88d3931
hydroperx <hydroperfox@gmail.com>
2025-04-27 09:13:23 -0300

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -4,55 +4,49 @@ import BaseDraggable from "@hydroperx/draggable";
 const at_browser = typeof window !== "undefined";
 
 let pointerMoveHandlers = [];
-function window_onPointerMove(e: PointerEvent): void
-{
-    for (const handler of pointerMoveHandlers)
-    {
-        handler(e);
-    }
+function window_onPointerMove(e: PointerEvent): void {
+  for (const handler of pointerMoveHandlers) {
+    handler(e);
+  }
 }
 window.addEventListener("pointermove", window_onPointerMove);
 
 let pointerUpHandlers = [];
-function window_onPointerUp(e: PointerEvent): void
-{
-    for (const handler of pointerUpHandlers)
-    {
-        handler(e);
-    }
+function window_onPointerUp(e: PointerEvent): void {
+  for (const handler of pointerUpHandlers) {
+    handler(e);
+  }
 }
 window.addEventListener("pointerup", window_onPointerUp);
 window.addEventListener("pointercancel", window_onPointerUp);
 
-export default function Draggable(options: DraggableOptions)
-{
-    let {
-        dragStart: drag_start,
-        dragMove: drag_move,
-        dragStop: drag_stop,
-        limit,
-        nodeRef: el_ref,
-        children,
-        disabled,
-    } = options;
+export default function Draggable(options: DraggableOptions) {
+  let {
+    dragStart: drag_start,
+    dragMove: drag_move,
+    dragStop: drag_stop,
+    limit,
+    nodeRef: el_ref,
+    children,
+    disabled,
+  } = options;
 
-    let draggable: BaseDraggable | null = null;
+  let draggable: BaseDraggable | null = null;
 
-    function createDraggable()
-    {
-        draggable = new BaseDraggable(el_ref.current!, {
-            limit,
+  function createDraggable() {
+    draggable = new BaseDraggable(el_ref.current!, {
+      limit,
 
-            onDragStart(element, x, y, event) {
-                drag_start?.({ element: element as HTMLElement, x, y });
-            },
+      onDragStart(element, x, y, event) {
+        drag_start?.({ element: element as HTMLElement, x, y });
+      },
 
-            onDrag(element, x, y, event) {
-                drag_move?.({ element: element as HTMLElement, x, y });
-            },
+      onDrag(element, x, y, event) {
+        drag_move?.({ element: element as HTMLElement, x, y });
+      },
 
-            onDragEnd(element, x, y, event) {
-                /*
+      onDragEnd(element, x, y, event) {
+        /*
                 if (finish_parent)
                 {
                     if (finish === "translate")
@@ -73,77 +67,72 @@ export default function Draggable(options: DraggableOptions)
                     }
                 }
                 */
-                drag_stop?.({ element: element as HTMLElement, x, y });
-            },
-        });
+        drag_stop?.({ element: element as HTMLElement, x, y });
+      },
+    });
+  }
+
+  useEffect(() => {
+    if (!disabled) {
+      if (draggable) draggable.destroy();
+      createDraggable();
     }
 
-    useEffect(() => {
-        if (!disabled)
-        {
-            if (draggable) draggable.destroy();
-            createDraggable();
-        }
+    // Cleanup
+    return () => {
+      if (draggable) draggable.destroy(), (draggable = null);
+    };
+  }, [disabled, limit]);
 
-        // Cleanup
-        return () => {
-            if (draggable) draggable.destroy(), draggable = null;
-        };
-    }, [disabled, limit]);
+  useEffect(() => {
+    // Cleanup
+    return () => {
+      if (draggable) draggable.destroy(), (draggable = null);
+    };
+  }, []);
 
-    useEffect(() => {
-        // Cleanup
-        return () => {
-            if (draggable) draggable.destroy(), draggable = null;
-        };
-    }, []);
-
-    return (<>
-        {children}
-    </>);
+  return <>{children}</>;
 }
 
 export type DraggableOptions = {
-    nodeRef: React.MutableRefObject<HTMLElement>,
-    limit?: HTMLElement,
-    children?: React.ReactNode,
-    disabled?: boolean,
-    rem?: number,
+  nodeRef: React.MutableRefObject<HTMLElement>;
+  limit?: HTMLElement;
+  children?: React.ReactNode;
+  disabled?: boolean;
+  rem?: number;
 
-    dragStart?: (data: DraggableData) => void,
-    dragMove?: (data: DraggableData) => void,
-    dragStop?: (data: DraggableData) => void,
+  dragStart?: (data: DraggableData) => void;
+  dragMove?: (data: DraggableData) => void;
+  dragStop?: (data: DraggableData) => void;
 };
 
 export type DraggableData = {
-    element: HTMLElement,
-    x: number,
-    y: number,
+  element: HTMLElement;
+  x: number;
+  y: number;
 };
 
 const utils = {
-    extractInsetOffsets(element: Element): { left: number, top: number }
-    {
-        const e = element as HTMLElement;
-        const { inset } = e.style;
-        if (!inset) return { left: 0, top: 0 };
-        const m = inset.match(/-?(?:\d*\.\d+|\d+)/g);
-        if (!m) return { left: 0, top: 0 };
-        return {
-            left: parseFloat(m[1]),
-            top: parseFloat(m[0]),
-        }
-    },
+  extractInsetOffsets(element: Element): { left: number; top: number } {
+    const e = element as HTMLElement;
+    const { inset } = e.style;
+    if (!inset) return { left: 0, top: 0 };
+    const m = inset.match(/-?(?:\d*\.\d+|\d+)/g);
+    if (!m) return { left: 0, top: 0 };
+    return {
+      left: parseFloat(m[1]),
+      top: parseFloat(m[0]),
+    };
+  },
 
-    extractLeftTopOffsets(element: Element): { left: number, top: number }
-    {
-        const e = element as HTMLElement;
-        const { left, top } = e.style;
-        const m_top = top ? top.match(/-?(?:\d*\.\d+|\d+)/) : "0";
-        const m_left = left ? left.match(/-?(?:\d*\.\d+|\d+)/) : "0";
-        return {
-            left: parseFloat(m_left ? m_left[0] : "0"),
-            top: parseFloat(m_top ? m_top[0] : "0"),
-        }
-    },
+  extractLeftTopOffsets(element: Element): { left: number; top: number } {
+    const e = element as HTMLElement;
+    const { left, top } = e.style;
+    const m_top = top ? top.match(/-?(?:\d*\.\d+|\d+)/) : "0";
+    const m_left = left ? left.match(/-?(?:\d*\.\d+|\d+)/) : "0";
+    return {
+      left: parseFloat(m_left ? m_left[0] : "0"),
+      top: parseFloat(m_top ? m_top[0] : "0"),
+    };
+  },
 };
